6b37672f49d388e5491c1548b3dd7eba30f8731e
kalidasupiriya <kalidasupiriya@gmail.com>
2025-08-19 12:20:26 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', // frontend URL
+  origin: 'http://localhost:5173, https://skygoal-henna.vercel.app/', // frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
